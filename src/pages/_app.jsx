ac3a84ab012a8fcf1993442f84268d2160acfb60
mum-never-proud <abhuz@hotmail.com>
2020-11-06 20:15:20 +0530

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -12,6 +12,19 @@ const App = ({ Component, pageProps }) => (
       <link href="./images/find-abith-hussain.svg" rel="mask-icon" color="#000" />
 
       <title>Find Abith Hussain</title>
+
+      <script
+        type="text/javascript"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+            ;(function () {
+              window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+              heap.load("3091194128");
+            })();
+          `,
+        }}
+      />
     </Head>
     <Header />
     <div className="container">
